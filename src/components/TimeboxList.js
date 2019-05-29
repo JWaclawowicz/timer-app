a12f6e83c2040192563421601533dc72588c754a
Justyna Wacławowicz <justyna.waclawowicz@gmail.com>
2019-05-29 23:06:19 +0200

    Use ErrorMessage to display error on adding timebox

--- src/components/TimeboxList.js
+++ src/components/TimeboxList.js
@@ -2,6 +2,7 @@ import React from "react";
 
 import Timebox from "./Timebox";
 import TimeboxCreator from "./TimeboxCreator";
+import ErrorMessage from "./ErrorMessage";
 
 class TimeboxList extends React.Component {
     state = {
@@ -9,9 +10,11 @@ class TimeboxList extends React.Component {
             { id: "a", title: "Uczę się o console", totalTimeInMinutes: 25 },
             { id: "b", title: "Uczę się debugować", totalTimeInMinutes: 15 },
             { id: "c", title: "Uczę się TDD", totalTimeInMinutes: 5 },
-        ]
+        ],
+        hasError: false
     }
     addTimebox = (timebox) => {
+        // throw new Error("Nie udało się utworzyć timeboxa");
         this.setState(prevState => {
             const timeboxes = [timebox, ...prevState.timeboxes];
             return { timeboxes };
@@ -36,12 +39,15 @@ class TimeboxList extends React.Component {
             this.addTimebox(createdTimebox);
         } catch (error) {
             console.log("Jest błąd przy tworzeniu timeboxa:", error);
+            this.setState({ hasError: true });
         }
     }
     render() {
+        const { hasError } = this.state;
         return (
             <>
                 <TimeboxCreator onCreate={this.handleCreate} />
+                <ErrorMessage hasError={hasError} message="Nie powiodło się dodanie nowego timeboxa" />
                 {
                     this.state.timeboxes.map((timebox, index) => (
                         <Timebox 
